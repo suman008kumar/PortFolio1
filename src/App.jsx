7c42e35d02c1cd61221c5c23454977a632f0c5d8
suman008kumar <54628850+suman008kumar@users.noreply.github.com>
2025-01-05 22:42:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
-import PortFolio from './components/portFolio'
+import PortFolio from "./components/PortFolio";
 import Experiance from './components/Experiance'
 import Footer from './components/Footer'
 import Contact from './components/Contact'
