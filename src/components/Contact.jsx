1741f318563168a2f197ce9dddcd2ef9d4c82df9
suman008kumar <54628850+suman008kumar@users.noreply.github.com>
2025-01-29 15:10:34 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ function Contact() {
       message: data.message,
     };
     try {
-      await axios.post("https://getform.io/f/ajjjwyda", userInfo);
+      await axios.post("https://getform.io/f/bgddklxa", userInfo);
       toast.success("Your message has been sent");
     } catch (error) {
       console.log(error);
